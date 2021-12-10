f81c0d4f139cdb08bb5f458ac1143db4008c0c74
Aviral Mehrotra <aviral.mehrotra@gmail.com>
2021-12-10 09:52:00 -0600

    Update methods.java

--- APCSASemester1Project/restaurantAppCreator/src/methods.java
+++ APCSASemester1Project/restaurantAppCreator/src/methods.java
@@ -5,14 +5,13 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 
-// Reformat the JSON so the name of each item is a key with the other inputs as values
-
-
 public class methods {
 
     private JSONArray ja = new JSONArray();
     private JSONArray swiftJA = new JSONArray();
-
+    
+    
+    // Creates a JSONObject for a menu item - for Python
     public JSONObject createItem(String type, double price, String url, String description) {
 
         JSONObject object = new JSONObject();
@@ -24,7 +23,7 @@ public class methods {
 
         return object;
     }
-
+    // Creates a JSONObject that is correctly formatted for Swift
     public JSONObject swiftJSONItem(int id, String name, String type, double price, String url, String description) {
 
         JSONObject object = new JSONObject();
@@ -39,7 +38,7 @@ public class methods {
         return object;
         
     }
-
+    // Adds the Python JSONObject to another object with the item name as a key and the object as the value
     public JSONObject finishItem(String name, JSONObject j) {
         JSONObject object = new JSONObject();
 
@@ -47,19 +46,19 @@ public class methods {
 
         return object;
     }
-
+    // Adds JSONObject to JSONArray
     public void addItem(JSONObject j) {
         ja.add(j);
     }
-
+    // Adds JSONObject to Swift JSONArray
     public void swiftAddItem(JSONObject j) {
         swiftJA.add(j);
     }
-
+    // Simply returns the Swift JSONArray
     public JSONArray returnJA() {
         return swiftJA;
     }
-
+    // Adds non-Swift JSONArray to JSONObject with the restaurant name as key and the JSONArray as the value
     public JSONObject mainJSON(String n) {
         JSONObject object = new JSONObject();
 
@@ -67,7 +66,7 @@ public class methods {
 
         return object;
     }
-
+    // Adds JSONObject to JSON file for Python
     public void addToFile(JSONObject jsonToAdd) {
 
         try {
@@ -78,7 +77,7 @@ public class methods {
             e.printStackTrace();
         }
     }
-
+    // Adds JSONObject to JSON file for Swift
     public void swiftAddToFile(JSONArray jArrayToAdd) {
         try {
             FileWriter file = new FileWriter("/Users/aviralmehrotra/Downloads/Code/Java/APCSASemester1Project/restaurantAppCreator/src/restaurantMenuAppTemplate/restaurantMenuAppTemplate/dataOutput.json");
